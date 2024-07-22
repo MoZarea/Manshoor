e60757651d30b1aaebab8440165022e90d5646ee
Mohamed Zarea <mohamedzarea29@gmail.com>
2024-07-22 13:51:53 +0300

    init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,5 +80,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.material.icons.extended)
     implementation(libs.landscapist.glide)
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+
 
 }
